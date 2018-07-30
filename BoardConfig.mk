5c2b396947fe33f9742d8bc9b2a0fcbe5042649c
Sujit Roy <kumarsujitroy@gmail.com>
2018-07-30 05:52:30 +0000

    ArrowOS bring-up
    Change-Id: I32286c86c11530b523224e977d04f764cd29a256
    Signed-off-by: Sujit Roy <kumarsujitroy@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -256,9 +256,8 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 VENDOR_SECURITY_PATCH := 2018-10-01
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_VERS := 28.0
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Use Snapdragon LLVM, if available
 TARGET_USE_SDCLANG := true

f88899a98aef968eaa7b3fc67f7acba0955257ac
Sonal Singh <sonal.singh.19993@gmail.com>
2019-03-29 13:38:14 +0000

    X01BD: Add Retro Music Player as prebuilt

--- device.mk
+++ device.mk
@@ -263,6 +263,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Music
 
+# Music
+PRODUCT_PACKAGES += \
+    Music
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
